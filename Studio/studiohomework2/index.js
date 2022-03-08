6b699e525e347c87424dce1483662c477ca1dfd0
graeme120 <graemem2414@gmail.com>
2022-03-08 13:40:41 -0500

    Added Studio Project

---
+++ Studio/studiohomework2/index.js
@@ -0,0 +1,33 @@
+
+const block = document.querySelector(".block")
+
+
+function changeFont(){
+
+    console.log(event.target.value)
+    const value= event.target.value 
+    const fontName = `Helvetica${value}`
+    console.log(fontName)
+    block.style.fontFamily= fontName
+}
+textFit(document.querySelector(".block"));
+
+const shareButton = document.querySelector('.share-button');
+
+shareButton.addEventListener('click', async () => {
+  const response = await fetch(`H${value}.png`);
+  const blob = await response.blob();
+  const filesArray = [
+    new File([blob], 'The Poor Image.png', {
+      type: 'image/png',
+      lastModified: new Date().getTime(),
+    }),
+  ];
+  const shareData = {
+    files: filesArray,
+  };
+  navigator.share(shareData);
+});
+
+
+
